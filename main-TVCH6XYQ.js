d58c0d97029d7cbe85f6ebfc1c9c542e084c87d6
rtallarr <89488066+rtallarr@users.noreply.github.com>
2024-08-21 03:18:49 +0000

    Deploying to gh-pages from @ rtallarr/Bob-angular@ae3ff9279f7fa1353ebe0947e101f1a10bf4cb55 🚀

---
+++ main-TVCH6XYQ.js
@@ -0,0 +1,165 @@
+import{U as A}from"./chunk-5J2BXDH6.js";import{a as k,c as x}from"./chunk-G2QSFCEH.js";import{b as O,c as _}from"./chunk-U7VMM5QD.js";import"./chunk-AESYL7IM.js";import{Da as m,Ob as M,Ta as u,V as c,Va as f,X as d,Xa as y,Za as v,_a as C,ba as p,cc as P,ga as g,ja as h,tb as b}from"./chunk-F3U37KQN.js";var w=[{path:"table",loadComponent:()=>import("./chunk-WMF3WVVA.js").then(r=>r.BlockListTableComponent)},{path:"blocklist",loadComponent:()=>import("./chunk-B3TQBSCX.js").then(r=>r.BlockListComponent)},{path:"**",loadComponent:()=>import("./chunk-QF5A4GSQ.js").then(r=>r.HomeComponent)}];var T="@",N=(()=>{let e=class e{constructor(t,o,i,a,l){this.doc=t,this.delegate=o,this.zone=i,this.animationType=a,this.moduleImpl=l,this._rendererFactoryPromise=null,this.scheduler=p(f,{optional:!0})}ngOnDestroy(){this._engine?.flush()}loadImpl(){return(this.moduleImpl??import("./chunk-XOG256KZ.js")).catch(o=>{throw new c(5300,!1)}).then(({\u0275createEngine:o,\u0275AnimationRendererFactory:i})=>{this._engine=o(this.animationType,this.doc,this.scheduler);let a=new i(this.delegate,this._engine,this.zone);return this.delegate=a,a})}createRenderer(t,o){let i=this.delegate.createRenderer(t,o);if(i.\u0275type===0)return i;typeof i.throwOnSyntheticProps=="boolean"&&(i.throwOnSyntheticProps=!1);let a=new s(i);return o?.data?.animation&&!this._rendererFactoryPromise&&(this._rendererFactoryPromise=this.loadImpl()),this._rendererFactoryPromise?.then(l=>{let F=l.createRenderer(t,o);a.use(F)}).catch(l=>{a.use(i)}),a}begin(){this.delegate.begin?.()}end(){this.delegate.end?.()}whenRenderingDone(){return this.delegate.whenRenderingDone?.()??Promise.resolve()}};e.\u0275fac=function(o){u()},e.\u0275prov=d({token:e,factory:e.\u0275fac});let r=e;return r})(),s=class{constructor(e){this.delegate=e,this.replay=[],this.\u0275type=1}use(e){if(this.delegate=e,this.replay!==null){for(let n of this.replay)n(e);this.replay=null}}get data(){return this.delegate.data}destroy(){this.replay=null,this.delegate.destroy()}createElement(e,n){return this.delegate.createElement(e,n)}createComment(e){return this.delegate.createComment(e)}createText(e){return this.delegate.createText(e)}get destroyNode(){return this.delegate.destroyNode}appendChild(e,n){this.delegate.appendChild(e,n)}insertBefore(e,n,t,o){this.delegate.insertBefore(e,n,t,o)}removeChild(e,n,t){this.delegate.removeChild(e,n,t)}selectRootElement(e,n){return this.delegate.selectRootElement(e,n)}parentNode(e){return this.delegate.parentNode(e)}nextSibling(e){return this.delegate.nextSibling(e)}setAttribute(e,n,t,o){this.delegate.setAttribute(e,n,t,o)}removeAttribute(e,n,t){this.delegate.removeAttribute(e,n,t)}addClass(e,n){this.delegate.addClass(e,n)}removeClass(e,n){this.delegate.removeClass(e,n)}setStyle(e,n,t,o){this.delegate.setStyle(e,n,t,o)}removeStyle(e,n,t){this.delegate.removeStyle(e,n,t)}setProperty(e,n,t){this.shouldReplay(n)&&this.replay.push(o=>o.setProperty(e,n,t)),this.delegate.setProperty(e,n,t)}setValue(e,n){this.delegate.setValue(e,n)}listen(e,n,t){return this.shouldReplay(n)&&this.replay.push(o=>o.listen(e,n,t)),this.delegate.listen(e,n,t)}shouldReplay(e){return this.replay!==null&&e.startsWith(T)}};function I(r="animations"){return v("NgAsyncAnimations"),h([{provide:y,useFactory:(e,n,t)=>new N(e,n,t,r),deps:[P,O,C]},{provide:m,useValue:r==="noop"?"NoopAnimations":"BrowserAnimations"}])}var S={providers:[x(w),I(),{provide:A,useValue:{duration:2500}}]};var R=(()=>{let e=class e{constructor(){this.title="TzTok-Bob"}};e.\u0275fac=function(o){return new(o||e)},e.\u0275cmp=g({type:e,selectors:[["app-root"]],standalone:!0,features:[M],decls:1,vars:0,template:function(o,i){o&1&&b(0,"router-outlet")},dependencies:[k],styles:[`[_nghost-%COMP%] {
+    --bright-blue: oklch(51.01% 0.274 263.83);
+    --electric-violet: oklch(53.18% 0.28 296.97);
+    --french-violet: oklch(47.66% 0.246 305.88);
+    --vivid-pink: oklch(69.02% 0.277 332.77);
+    --hot-red: oklch(61.42% 0.238 15.34);
+    --orange-red: oklch(63.32% 0.24 31.68);
+
+    --gray-900: oklch(19.37% 0.006 300.98);
+    --gray-700: oklch(36.98% 0.014 302.71);
+    --gray-400: oklch(70.9% 0.015 304.04);
+
+    --red-to-pink-to-purple-vertical-gradient: linear-gradient(
+      180deg,
+      var(--orange-red) 0%,
+      var(--vivid-pink) 50%,
+      var(--electric-violet) 100%
+    );
+
+    --red-to-pink-to-purple-horizontal-gradient: linear-gradient(
+      90deg,
+      var(--orange-red) 0%,
+      var(--vivid-pink) 50%,
+      var(--electric-violet) 100%
+    );
+
+    --pill-accent: var(--bright-blue);
+
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1[_ngcontent-%COMP%] {
+    font-size: 3.125rem;
+    color: var(--gray-900);
+    font-weight: 500;
+    line-height: 100%;
+    letter-spacing: -0.125rem;
+    margin: 0;
+    font-family: "Inter Tight", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+
+  p[_ngcontent-%COMP%] {
+    margin: 0;
+    color: var(--gray-700);
+  }
+
+  main[_ngcontent-%COMP%] {
+    width: 100%;
+    min-height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    box-sizing: inherit;
+    position: relative;
+  }
+
+  .angular-logo[_ngcontent-%COMP%] {
+    max-width: 9.2rem;
+  }
+
+  .content[_ngcontent-%COMP%] {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    max-width: 700px;
+    margin-bottom: 3rem;
+  }
+
+  .content[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%] {
+    margin-top: 1.75rem;
+  }
+
+  .content[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+    margin-top: 1.5rem;
+  }
+
+  .divider[_ngcontent-%COMP%] {
+    width: 1px;
+    background: var(--red-to-pink-to-purple-vertical-gradient);
+    margin-inline: 0.5rem;
+  }
+
+  .pill-group[_ngcontent-%COMP%] {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    flex-wrap: wrap;
+    gap: 1.25rem;
+  }
+
+  .pill[_ngcontent-%COMP%] {
+    display: flex;
+    align-items: center;
+    --pill-accent: var(--bright-blue);
+    background: color-mix(in srgb, var(--pill-accent) 5%, transparent);
+    color: var(--pill-accent);
+    padding-inline: 0.75rem;
+    padding-block: 0.375rem;
+    border-radius: 2.75rem;
+    border: 0;
+    transition: background 0.3s ease;
+    font-family: var(--inter-font);
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.4rem;
+    letter-spacing: -0.00875rem;
+    text-decoration: none;
+  }
+
+  .pill[_ngcontent-%COMP%]:hover {
+    background: color-mix(in srgb, var(--pill-accent) 15%, transparent);
+  }
+
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 1) {
+    --pill-accent: var(--bright-blue);
+  }
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 2) {
+    --pill-accent: var(--french-violet);
+  }
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 3), .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 4), .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 5) {
+    --pill-accent: var(--hot-red);
+  }
+
+  .pill-group[_ngcontent-%COMP%]   svg[_ngcontent-%COMP%] {
+    margin-inline-start: 0.25rem;
+  }
+
+  .social-links[_ngcontent-%COMP%] {
+    display: flex;
+    align-items: center;
+    gap: 0.73rem;
+    margin-top: 1.5rem;
+  }
+
+  .social-links[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+    transition: fill 0.3s ease;
+    fill: var(--gray-400);
+  }
+
+  .social-links[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover   svg[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+    fill: var(--gray-900);
+  }
+
+  @media screen and (max-width: 650px) {
+    .content[_ngcontent-%COMP%] {
+      flex-direction: column;
+      width: max-content;
+    }
+
+    .divider[_ngcontent-%COMP%] {
+      height: 1px;
+      width: 100%;
+      background: var(--red-to-pink-to-purple-horizontal-gradient);
+      margin-block: 1.5rem;
+    }
+  }`]});let r=e;return r})();_(R,S).catch(r=>console.error(r));
