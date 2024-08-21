ae3ff9279f7fa1353ebe0947e101f1a10bf4cb55
Rodrigo Tallar <rtallarr@gmail.com>
2024-08-20 23:18:03 -0400

    fix: link to go to block list page

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
